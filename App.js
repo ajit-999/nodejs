44f90b1bd909d81af81365cd7dbd5e565ed75791
ajit-999 <ajitkumarjain123@gmail.com>
2017-10-08 09:22:34 +0530

    Creating Server

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-var printstuff = function(stuff){
+/*var printstuff = function(stuff){
 	console.log(stuff);
 }
 
@@ -9,8 +9,37 @@ function mainfunction(anotherfunction,value){
 mainfunction(printstuff,"Ajit Jain");
 
 function printstuff1(){
-	console.log("THis is set timeout");
+	console.log("This is set timeout");
 }
 setTimeout(printstuff1,5000);
-setInterval(printstuff1,2000);
+//setInterval(printstuff1,2000);
+
+console.log("User 1 made a request");
+setTimeout(callback,5000);
+
+console.log("User 2 made a request");
+setTimeout(callback,5000);
+
+console.log("User 2 made a request");
+setTimeout(callback,5000);
+
+function callback(){
+	console.log("Queried the databse amd deleiverd data in 5 seconds");
+}
+
+
+var athelete = require('./athelete.js');
+athelete.relay(); 
+athelete.longjump();
+*/
+var http = require('http');
+	http.createServer(function(request,response){
+      response.writeHead(200,{"Content-Type":"text/plain"});
+      response.write("Ajit Jain");
+      response.end();
+	}).listen(5000);
+
+
+ 
+
 
---
+++ athelete.js
@@ -0,0 +1,17 @@
+/*function relay(){
+	console.log("THis is rely function");
+}
+function longjump(){
+	console.log("this is Long Jump function"); 
+}
+module.exports.relay = relay ;
+module.exports.longjump = longjump;*/
+
+module.exports = {
+	relay:function(){
+		console.log("This is relay function");
+	},
+	longjump: function(){
+		console.log("THis is longjump function");
+	}
+} 
